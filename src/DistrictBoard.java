a526421b627ffbb3679805b0d12a2abbff66d643
Andrew Marshall <marshall.a.f@gmail.com>
2016-10-31 00:27:19 -0500

    Add: constructor declarations for DistrictBoard

--- src/DistrictBoard.java
+++ src/DistrictBoard.java
@@ -1,4 +1,36 @@
 
 public class DistrictBoard {
-
+    
+    // holds the board
+    private byte[][] board;
+    
+    /**
+     * Initializes a random M x N board, with a random ratio of red/blue.
+     * 
+     * @param m height of board
+     * @param n width of board
+     */
+    public DistrictBoard(int m, int n) {
+        
+    }
+    
+    /**
+     * Initializes a random M x N board, with the given ratio of red/blue.
+     * 
+     * @param m height of board
+     * @param n width of board
+     * @param ratio ratio of red/blue
+     */
+    public DistrictBoard(int m, int n, float ratio) {
+        
+    }
+    
+    /**
+     * Initializes a board from the given file.
+     * 
+     * @param filename
+     */
+    public DistrictBoard(String filename) {
+        
+    }
 }
