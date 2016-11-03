070ed44603ac3d79541d6ff6bbea493fa006d92b
Andrew Marshall <marshall.a.f@gmail.com>
2016-11-02 23:37:53 -0500

    Add: method to return possible move coordinates

--- src/BoardPermute.java
+++ src/BoardPermute.java
@@ -42,6 +42,40 @@ public class BoardPermute {
     }
     
     public ArrayList<Point> nextMoves(int activeDistrict) {
+        ArrayList<Point> moves = new ArrayList<>();
+        // array for keeping track of the squares we've checked
+        boolean[][] checked = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (possibleBoard[i][j] != 0) {
+                    checked[i][j] = true;
+                } else checked[i][j] = false;
+            }
+        }
         
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (possibleBoard[i][j] == activeDistrict) {
+                    if (i != 0 && !checked[i-1][j]) {
+                        moves.add(new Point(i-1, j));
+                        checked[i-1][j] = true;
+                    }
+                    else if (i != rows-1 && !checked[i+1][j]) {
+                        moves.add(new Point(i+1, j));
+                        checked[i+1][j] = true;
+                    }
+                    else if (j != 0 && !checked[i][j-1]) {
+                        moves.add(new Point(i, j-1));
+                        checked[i][j-1] = true;
+                    }
+                    else if (j != cols-1 && !checked[i][j+1]) {
+                        moves.add(new Point(i, j+1));
+                        checked[i][j+1] = true;
+                    }
+                }
+            }
+        }
+        
+        return moves;
     }
 }
