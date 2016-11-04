dc19e77f7065b1bcf3fe8b62e71ed01889d39757
Andrew Marshall <marshall.a.f@gmail.com>
2016-11-03 22:47:23 -0500

    Add: graph methods

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,28 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Graph<Node> {
     private Map<Node, Set<Node>> map = new HashMap<>();
     
+    public void addNode(Node node) {
+        map.put(node, new HashSet<Node>());
+    }
+    
+    public void addEdge(Node node1, Node node2) {
+        map.get(node1).add(node2);
+        map.get(node2).add(node1);
+    }
+    
+    public void removeNode(Node node) {
+        Set<Node> adj = getAdjacent(node);
+        for (Node adjNode : adj) {
+            map.get(adjNode).remove(node);
+        }
+    }
+    
+    public Set<Node> getAdjacent(Node node) {
+        return map.get(node);
+    }
 }
